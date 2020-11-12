102788ec190934067b38773fd06e80b198034b6e
Liu Xing <chn.liuxing@gmail.com>
2020-11-12 15:35:20 +0800

    perf: optimization package size

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,20 @@ let config = {
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom"
+    },
+  },
   module: {
     rules: [
       {
